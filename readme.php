df90a6a2693cb769c2ea5c35a904c02ba2199e93
jclavo <jclavotafur@gmail.com>
2021-08-28 12:09:31 -0300

    set commands

--- readme.php
+++ readme.php
@@ -1,3 +1,17 @@
-# Run a file
+# How to run it 
 
-- php [path-file.php]
+- clone the repository
+- docker-compose build
+- docker-compose up -d
+- docker exec -it myphp-7.4 /bin/bash
+- php [path-file.php] (php samples/hello/hello.php)  
+
+
+
+# Acess Apache by browser (port:808, it can be modified)
+
+- http://localhost:808/ (default index)
+
+- http://localhost:808/[path-to-file.php] (specific file)
+- http://localhost:808/samples/hello/hello.php
+
